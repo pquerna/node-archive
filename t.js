5f2989e10dad9beac1d9f5bde009c2ae82adc471
Paul Querna <pquerna@apache.org>
2010-06-29 01:00:58 -0700

    refactor t.js a bit to be more like what I want the api to look like

--- t.js
+++ t.js
@@ -1,5 +1,29 @@
-var a = require('./archive_bindings');
-var b = new a.ArchiveReader();
-var sys = require('sys')
-b.open_file("nofile.tar.gz", function(err){sys.log('hit it in error handler'); sys.log(err);})
+var ab = require('./archive_bindings');
+var ar = new ab.ArchiveReader();
+var sys = require('sys');
+
+tf.addListener('ready', function() {
+  sys.log('In ready function....');
+  tf.next();
+});
+
+tf.addListener('entry', function(entry) {
+  sys.log('path: '+ entry.getPath());
+  sys.log('    size: '+ entry.getSize());
+  sys.log('    mtime: '+ entry.getMtime());
+/*
+  estream = entry.createStream()
+  estream.addListener('data', function(data) { });
+
+  estream.addListener('end', function() { });
+*/
+  /* TODO: is the right api? */
+  tf.next();
+});
+
+b.openFile("nofile.tar.gz", function(err){
+  if (err) {
+    sys.log(err);
+  }
+});
 
